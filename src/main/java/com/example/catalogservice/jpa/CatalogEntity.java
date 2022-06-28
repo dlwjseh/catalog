ef7c69f7c9970af8b98a994e4bb7da5e013d01fb
LeeJeondo <jeondo.lee@kbfg.com>
2022-06-28 14:43:56 +0900

    init

---
+++ src/main/java/com/example/catalogservice/jpa/CatalogEntity.java
@@ -0,0 +1,39 @@
+package com.example.catalogservice.jpa;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Data
+@Entity
+@Table(name = "catalog")
+public class CatalogEntity implements Serializable {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, length = 120, unique = true)
+	private String productId;
+
+	@Column(nullable = false)
+	private String productName;
+
+	@Column(nullable = false)
+	private Integer stock;
+
+	@Column(nullable = false)
+	private Integer unitPrice;
+
+	@Column(columnDefinition = "DATETIME", nullable = false, updatable = false, insertable = false)
+	@ColumnDefault("CURRENT_TIMESTAMP")
+	private Date createdAt;
+}
